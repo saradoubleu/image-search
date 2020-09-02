3bdf3158b1e67ec5a9bb521a3698449a2b27431f
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-09-02 03:51:53 -0400

    feat: user-agent not working

--- src/api/shutterstock.js
+++ src/api/shutterstock.js
@@ -6,7 +6,8 @@ const instance = axios.create({
   baseURL: baseURL,
   timeout: 1000,
   headers: {
-    // "User-Agent": "stwomi",
+    //   "User-Agent":
+    //     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
     Authorization: `Bearer ${process.env.REACT_APP_SHUTTERSTOCK_API_TOKEN}`,
   },
 });
