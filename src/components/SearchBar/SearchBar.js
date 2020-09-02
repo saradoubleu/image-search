944138114d1fb3a1f0864915dc5a03b0045b884f
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-09-02 02:53:51 -0400

    feat: submit button and function

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,12 @@
 import React, { Component, useState } from "react";
+import axios from "../../api/shutterstock";
 import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
 
 class SearchBar extends Component {
   state = { searchTerm: "" };
@@ -22,6 +29,36 @@ class SearchBar extends Component {
               value={this.state.searchTerm}
               onChange={(e) => this.setState({ searchTerm: e.target.value })}
             ></TextField>
+          </span>
+          {/* <span>
+            <FormControl variant="outlined">
+              <InputLabel id="demo-simple-select-outlined-label">
+                Age
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value="test"
+                // onChange={handleChange}
+                label="Category"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </span> */}
+          <span>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(event) => this.onFormSubmit(event)}
+            >
+              Search
+            </Button>
           </span>
         </form>
       </div>
