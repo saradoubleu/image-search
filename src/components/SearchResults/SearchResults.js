900ea2e5f4752e81dcccb6550af1333db3112114
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-09-02 03:55:45 -0400

    refactor: removed ternary implemented more readable alt

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,11 +2,9 @@ import React from "react";
 import ImageBlock from "./ImageBlock";
 
 const SearchResults = (props) => {
-  const images = props.images.data
-    ? props.images.data.map((image) => {
-        return <ImageBlock key={image.id} image={image} />;
-      })
-    : [];
+  const images = (props.images.data || []).map((image) => {
+    return <ImageBlock key={image.id} image={image} />;
+  });
 
   return <div className="image-gallery"> {images} </div>;
 };
